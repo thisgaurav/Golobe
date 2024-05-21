cc483d00bd108b157d7d2fba381229598cae00aa
thisgaurav <gauravgargpctebtech20@gmail.com>
2024-05-21 11:34:56 +0530

    Project Setup

---
+++ Golobe-FE/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../component-styles/styles.css'
+function Header() {
+  return (
+    <div className='flex justify-center mt-4'>
+      <nav className=' w-full flex px-20 py-6 justify-between items-center'>
+        <div className='flex gap-9'>
+          <div>
+            <a className='flex gap-2 typo items-center text-center' href="">
+              <img src='./images/airplane.png' />
+              <p>Find Flight</p>
+            </a>
+          </div>
+          <div>
+            <a href="" className='flex gap-2 items-center text-center typo'>
+              <img src='./images/ion_bed.png' />
+              <p>Find Stays</p>
+            </a>
+          </div>
+        </div>
+        <div className='flex items-center justify-center mt-3'>
+          <img src="./images/logo-black.png" alt="logo" />
+        </div>
+        <div className='flex gap-10'>
+          <button className='typo'>Login</button>
+          <button className='typo bg-black text-white rounded-xl p-4'>Sign-Up</button>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
--- README.md
+++
@@ -1,2 +0,0 @@
-# Golobe
-A blogging website for tourism and travel based in MERN stack
