94d38a58520c2f2029fb6178ead76c19814a6d15
AmanKumar2226 <aman.k@infiscale.io>
2024-05-23 11:58:33 +0530

    Routes implemented

--- Golobe-FE/src/App.jsx
+++ Golobe-FE/src/App.jsx
@@ -1,17 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import FlightListing from './components/FlightListing'
 import FlightSearch from './components/FlightSearch'
-import Footer from './components/Footer'
-import Header from './components/Header'
 import Home from './components/Home'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
 
   return (
     <>
-      {/* <FlightSearch/> */}
-      <FlightListing/>
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/flight-list' element={<FlightListing/>}/>
+        <Route path='/flight-search' element={<FlightSearch/>}/>
+      </Routes>
     </>
   )
 }
