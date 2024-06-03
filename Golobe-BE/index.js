a1ec37a92cb83c1a00a521eafeef36b29988ea53
AmanKumar2226 <aman.k@infiscale.io>
2024-06-03 11:44:07 +0530

    API created to getuser through token and token created as well

--- Golobe-BE/index.js
+++ Golobe-BE/index.js
@@ -2,47 +2,132 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import jsonwebtoken from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import cookieParser from "cookie-parser";
+import fetchUser from './middlewares/fetchUser.js'
 
+//Connection to database
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/golobe");
   console.log("db connected");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 main().catch((err) => console.log(err));
+
+//schema creation for user data
 const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   email: String,
   phoneNumber: String,
   password: String,
-  confirmPassword: String,
 });
 
+//model creation in mongodb
 const User = mongoose.model("User", userSchema);
 
+//creating server with express
 const server = express();
 server.use(cors());
 server.use(bodyParser.json());
+server.use(cookieParser());
 
+//getting data from frontend and assigning it to user model's variables
 server.post("/api/golobe", async (req, res) => {
-  let user = new User();
-  user.firstName = req.body.firstName;
-  user.lastName = req.body.lastName;
-  user.email = req.body.email;
-  user.password = req.body.password;
-  user.confirmPassword = req.body.confirmPassword;
-  user.phoneNumber = req.body.phoneNumber;
-  const doc = await user.save();
+  try {
+    let { firstName, lastName, email, password, phoneNumber } = req.body;
+    if (!(firstName && lastName && email && password && phoneNumber)) {
+      res.status(400).send("All fields are required");
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(401).send("The user with this email already exists");
+    }
 
-  console.log(doc);
-  res.json(doc);
+    const myEncPassword = await bcrypt.hash(password, 10);
+
+    let user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: myEncPassword,
+      phoneNumber,
+    });
+
+    const jwt = jsonwebtoken;
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+      },
+      "shhhh"
+    );
+    user.token = token;
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-server.get("/api/golobe", async (req, res) => {
-  const docs = await User.find({});
-  res.json(docs);
+
+//Login api using Post method
+server.post("/api/golobe/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!(email && password)) {
+      res.status(400).send("send all data");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(401).send("User not found");
+    }
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const jwt = jsonwebtoken;
+      const token = jwt.sign(
+        {
+          id: user._id,
+        },
+        "shhhh"
+      );
+      user.token = token;
+      console.log(user.token);
+
+      const options = {
+        httpOnly: true,
+      };
+      res.status(200).json({
+        success: true,
+        token,
+        user,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+
+//Getting logged in user details using POST: 'api/golobe/getUser'. Login required
+server.post('/api/golobe/get-user',fetchUser, async(req,res)=>{
+
+  try {
+    const userId = req.user;
+    const user = await User.findById(userId.id)
+    console.log(user,'ppppppppppppppppppppp')
+    return res.status(200).json({
+      success: true,
+      user
+    });
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
+//setting port number and listening on port 8080
 server.listen(8080, () => {
   console.log("server started");
 });
---
+++ Golobe-BE/middlewares/fetchUser.js
@@ -0,0 +1,21 @@
+
+import jsonwebtoken from "jsonwebtoken";
+
+const fetchUser = (req, res, next)=>{
+    //get the user from the jwt token and add id to request object
+    const token = req.header('token');
+    if(!token){
+        res.status(401).send({error: 'Please authenticate using a valid token'})
+    }
+    try {
+        const data = jsonwebtoken.verify(token, "shhhh"); // Replace with your secret key
+        console.log('Decoded token data:', data); // Log for debugging
+        req.user = data;
+        next();
+      } catch (err) {
+        console.error('Error verifying token:', err); // Log error for debugging
+        return res.status(401).send({ error: 'Please authenticate using a valid token' });
+      }
+    };
+
+export default fetchUser
